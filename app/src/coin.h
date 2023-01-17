2122e0740942add35e66a09f9fdcfe0332bc026f
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-01-17 21:12:24 +0100

    Rebrand to "Station"

--- app/src/coin.h
+++ app/src/coin.h
@@ -38,7 +38,7 @@ typedef enum {
 #define VIEW_ADDRESS_OFFSET_SECP256K1       PK_LEN_SECP256K1
 #define VIEW_ADDRESS_LAST_PAGE_DEFAULT      0
 
-#define MENU_MAIN_APP_LINE1                "Terra"
+#define MENU_MAIN_APP_LINE1                "Station"
 #define MENU_MAIN_APP_LINE2                "Ready"
 #define APPVERSION_LINE1                   "Version:"
 #define APPVERSION_LINE2                   ("v" APPVERSION)
